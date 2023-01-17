3a0296297f21c8aea1610c4a8e843c1e12295447
huisu <huisu0429@naver.com>
2023-01-17 22:35:05 +0900

    Add theme

---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from 'styled-components';
+export const Theme: DefaultTheme = {
+  bgColor: '#95afc0',
+  textColor: '#30336b',
+  accentColor: '#eb3b5a',
+  cardBgColor: '#F5F5F5',
+};
